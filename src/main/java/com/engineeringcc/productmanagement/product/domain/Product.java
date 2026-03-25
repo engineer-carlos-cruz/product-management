384e9782611851af5dd83ad9ae84c94e48f08081
Carlos Cruz <carturo99@gmail.com>
2026-03-25 09:11:29 -0500

    Add product domain model

---
+++ src/main/java/com/engineeringcc/productmanagement/product/domain/Product.java
@@ -0,0 +1,112 @@
+package com.engineeringcc.productmanagement.product.domain;
+
+import java.util.Objects;
+
+public class Product {
+
+    private final Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String image;
+
+    public Product(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.description = builder.description;
+        this.price = builder.price;
+        this.image = builder.image;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private Long id;
+        private String name;
+        private String description;
+        private Double price;
+        private String image;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder price(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder image(String image) {
+            this.image = image;
+            return this;
+        }
+
+        public Product build() {
+            if (Objects.isNull(name) || name.isEmpty()) {
+                throw new IllegalStateException("Debe proporcionar un nombre XXXXXXXX");
+            }
+            return new Product(this);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
