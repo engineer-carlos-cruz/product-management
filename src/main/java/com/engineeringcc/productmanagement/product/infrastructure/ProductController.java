fc1fba08ec6adba96a52f066648d9e6876616782
Carlos Cruz <carturo99@gmail.com>
2026-03-29 21:20:16 -0500

    Refactor product controller to dispatch create command

---
+++ src/main/java/com/engineeringcc/productmanagement/product/infrastructure/ProductController.java
@@ -0,0 +1,42 @@
+package com.engineeringcc.productmanagement.product.infrastructure;
+
+import com.engineeringcc.productmanagement.common.mediator.Mediator;
+import com.engineeringcc.productmanagement.product.application.CreateProductRequest;
+import com.engineeringcc.productmanagement.product.domain.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "products")
+public class ProductController {
+
+    private final Mediator mediator;
+
+    public ProductController(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        return ResponseEntity.ok(List.of());
+    }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<Product> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(Product.builder().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody CreateProductRequest request) {
+        mediator.dispatch(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping(path = "{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+    }
+}
