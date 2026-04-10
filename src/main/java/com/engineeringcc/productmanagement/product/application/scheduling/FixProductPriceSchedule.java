0a5b42de089fe4a73c3cec11a423784aa03c9ca5
Carlos Cruz <carturo99@gmail.com>
2026-04-10 17:50:17 -0500

    feat(products): split price correction schedules and run daily job at 17:50

--- src/main/java/com/engineeringcc/productmanagement/product/application/scheduling/FixProductPriceSchedule.java
+++ src/main/java/com/engineeringcc/productmanagement/product/application/scheduling/FixProductPriceSchedule.java
@@ -17,14 +17,30 @@ public class FixProductPriceSchedule {
     }
 
     @Scheduled(fixedRate = 5000)
-    public void fixProductsPrice() {
-        log.warn("Fixing price by all products...");
+    public void fixProductsPriceSeconds() {
+        log.warn("Fixing price by all products each 5 seconds...");
 
         repository.findAll().forEach(product -> {
-            product.setPrice(product.getPrice() * 1.1);
+            product.setPrice(product.getPrice() * 1.001);
             repository.save(product);
         });
 
+        finishLog();
+    }
+
+    @Scheduled(cron = "0 50 17 * * *")
+    public void fixProductsPriceDaily() {
+        log.warn("Fixing price by all products daily at 05:50 p.m...");
+
+        repository.findAll().forEach(product -> {
+            product.setPrice(product.getPrice() * 1.01);
+            repository.save(product);
+        });
+
+        finishLog();
+    }
+
+    private void finishLog() {
         log.info("Finished fixing products price.");
     }
 }
